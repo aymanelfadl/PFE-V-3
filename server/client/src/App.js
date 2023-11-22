d465ea847be1c51edea77129f029481d80cef91a
aymanelfadl <aymanelfadl16@gmail.com>
2023-11-22 20:18:44 +0100

    end of login / registration

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,4 +1,4 @@
-// App.js
+
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
--- server/client/src/components/Auth/Auth.js
+++ server/client/src/components/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./Auth.css"
 
 const AuthForm = () => {
 
